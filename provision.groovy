efb1db6fa6635ebf99f7c038882475e5c0cb23c3
github-empty <59059889+github-empty@users.noreply.github.com>
2019-12-27 16:07:53 +0200

    Update provision.groovy

--- provision.groovy
+++ provision.groovy
@@ -2,7 +2,7 @@ import jenkins.model.Jenkins
 
 Jenkins jenkins = Jenkins.instance
 
-String repository = "https://github.com/Deflier/edp-remove-pipelines-scripts"
+String repository = "https://github.com/github-empty/edp-remove-pipelines-scripts"
 
 String removePipelinesFolderName = "remove-pipelines"
 def removePipelinesFolder = jenkins.getItem(removePipelinesFolderName)
